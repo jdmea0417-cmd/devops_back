c5692b880ca643d65044ce1bd47f1dc9a7bf1460
jdmea <jdmea0417@gmail.com>
2026-07-02 16:56:51 +0900

    test

--- src/main/java/com/example/springBaseCode/SpringBaseCodeApplication.java
+++ src/main/java/com/example/springBaseCode/SpringBaseCodeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBaseCodeApplication {
-
+	// 안녕!
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBaseCodeApplication.class, args);
 	}
